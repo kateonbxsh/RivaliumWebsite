a272274c569a6fe2dd094dc898e8b279c61df2b0
Aouab <aouabsss@gmail.com>
2024-08-29 15:08:57 +0200

    implement new homepage navbar

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,12 +3,13 @@ import { MouseEventHandler } from "react";
 interface ButtonProps {
     type?: 'main' | 'secondary';
     content?: string;
+    small?: boolean;
     onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
-export default function Button({ type, onClick, content }: ButtonProps) {
+export default function Button({ type, onClick, content, small }: ButtonProps) {
 
-    return (<button className={"button " + (type ?? "")} onClick={onClick}>
+    return (<button className={"button " + (type ?? "") + (small ? " s" : "")} onClick={onClick}>
         {content}
     </button>)
 
--- src/pages/main/Navbar.tsx
+++ src/pages/main/Navbar.tsx
@@ -1,30 +1,42 @@
 import '../../style/pages/main/navbar.scss'
 import NavbarButton from "../../components/navbar/NavbarButton";
 import { useState } from 'react';
-import { useSpring, animated } from 'react-spring';
+import Button from '../../components/Button';
+import { useNavigate } from 'react-router-dom';
 const Icon = require("../../assets/images/logo/icon/icon.webp");
 
 export default function Navbar() {
 
+    const navigate = useNavigate();
     const [isOpen, setIsOpen] = useState(false);
 
     const handleLogoClick = () => {
         setIsOpen(!isOpen);
     };
 
-    return (<animated.div className={"navbar " + (isOpen ? "open" : "")}>
-        <a href="#" onClick={handleLogoClick}>
-            <img className="navbar-logo" src={Icon} alt="Rivalium Icon"/>
-        </a>
-        {isOpen && (
-        <div className="navbar-button-list">
-            <NavbarButton text="Home" href="/"/>
-            <NavbarButton text="Play" href="/download"/>
-            <NavbarButton text="Rivals" href="/rivals"/>
-            <NavbarButton text="Gameplay" href="/gameplay"/>
-            <NavbarButton text="About"  href="/game"/>
+    return (
+        <div className="navbar-container">
+            <div className={"navbar " + (isOpen ? "open" : "closed")}>
+                <a href="#" onClick={handleLogoClick}>
+                    <img className="navbar-logo" src={Icon} alt="Rivalium Icon"/>
+                </a>
+                {isOpen && (<>
+                    <div className="navbar-button-list">
+                        <NavbarButton text="Home" href="/"/>
+                        <NavbarButton text="Play" href="/download"/>
+                        <NavbarButton text="Rivals" href="/rivals"/>
+                        <NavbarButton text="Gameplay" href="/gameplay"/>
+                        <NavbarButton text="About"  href="/game"/>
+                    </div>
+                    <div className="navbar-portal">
+                        <Button small type="main" content="LOGIN" onClick={()=>navigate("/portal")}/>
+                        <Button small content="SIGNUP" onClick={()=>navigate("/portal")}/>
+                    </div>
+                </>
+                )}
+                
+            </div>
         </div>
-      )}
-    </animated.div>);
+    );
 
 }
